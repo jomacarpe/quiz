17b16e81af7d15aae3602cd1e27c6b3ab3ef9cc4
jomacarpe <jomacarpe@gmail.com>
2016-04-27 00:42:39 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var quizController = require('../controllers/quiz_controller')
+var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
+});
+router.get('/author', function(req, res, next) {
+  res.render('author'/*, { title: 'Quiz' }*/);
 });
 router.get('/question', quizController.question);
 router.get('/check', quizController.check);
