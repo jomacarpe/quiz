1a0d8f2f9de9e94a8ef07595185747717f16418f
jomacarpe <jomacarpe@gmail.com>
2016-05-31 01:21:54 +0200

    Tema 17

--- models/index.js
+++ models/index.js
@@ -17,7 +17,10 @@ var sequelize = new Sequelize(url,{storage: storage,
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-
+var Comment = sequelize.import(path.join(__dirname,'comment'));
+// Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 /*
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync()
@@ -42,3 +45,4 @@ sequelize.sync()
 
 */
 exports.Quiz = Quiz; // exportar definición de tabla Quiz 
+exports.Comment = Comment; // exportar definición de tabla Comments
