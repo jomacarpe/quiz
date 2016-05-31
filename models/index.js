82b4e75b0e2c667849877d67812f0949789aaa1d
jomacarpe <jomacarpe@gmail.com>
2016-05-31 14:20:52 +0200

    tema 19

--- models/index.js
+++ models/index.js
@@ -18,31 +18,11 @@ var sequelize = new Sequelize(url,{storage: storage,
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 var Comment = sequelize.import(path.join(__dirname,'comment'));
+var User = sequelize.import(path.join(__dirname,'user'));
 // Relaciones entre modelos
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
-/*
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync()
-    .then(function() {
-        // Ya se han creado las tablas necesarias.
-        return Quiz.count()
-                .then(function (c) {
-                    if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.bulkCreate([ {question: 'Capital de Francia',   answer: 'Paris'},
-                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
-                                              ])
-                                   .then(function() {
-                                        console.log('Base de datos inicializada con datos');
-                                    });
-                    }
-                });
-    })
-    .catch(function(error) {
-        console.log("Error Sincronizando las tablas de la BBDD:", error);
-        process.exit(1);
-    });
 
-*/
 exports.Quiz = Quiz; // exportar definición de tabla Quiz 
 exports.Comment = Comment; // exportar definición de tabla Comments
+exports.User = User;
